624db93cbd52f64ca6a7d9f503da519c86fd77c7
Yacin Tmimi <yacintmimi@gmail.com>
2018-11-03 19:53:18 -0400

    added for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+#add info to README.md
+# with open("README.md", "r") as f:
+#     long_description = f.read()
+
+setuptools.setup(
+    name="Graphene-ObjectType-Generator",
+    version="0.0.1",
+    author="Yacin Tmimi",
+    author_email="yacintmimi@gmail.com",
+    description="Quickly generate Graphene ObjectType definitions from dictionaries, lists, or JSON strings.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=['test']),
+    install_requires=['jinja2>=2.10'],
+    extras_require={
+        'dev': ['coverage', 'pytest'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
