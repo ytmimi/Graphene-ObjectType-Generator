54b6b4614601174f6889bf1d02dbf66ac9615308
Yacin Tmimi <yacintmimi@gmail.com>
2018-11-03 22:11:54 -0400

    includes top level imports

--- objecttype_generator/__init__.py
+++ objecttype_generator/__init__.py
@@ -1,6 +1,2 @@
-# from .templates import graphene_field, graphene_scalar, graphene_list, resolver, objecttype
-# from file_generator import (object_type_to_file,
-#     object_type_from_json_response, objectType_file_from_request)
-#
-from objectType import objectTypes_from_dict, create_objectType
+from .objecttype import from_dict, from_list, from_json
 __name__ = 'objecttype_generator'
