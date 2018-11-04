5ddac85b9d2710540e9a77e676ab7f0595d2c765
Yacin Tmimi <yacintmimi@gmail.com>
2018-11-04 12:01:46 -0500

    updated to_py_file_function

--- objecttype_generator/file_generator.py
+++ objecttype_generator/file_generator.py
@@ -2,14 +2,14 @@ def imports(*args):
     '''args: properly formated import strings'''
     return '\n'.join(args)
 
-def to_py_file(full_file_name, mode, objectType):
+def to_py_file(full_file_name, objecttype, mode='w', imports=None):
     '''
     writes the contents of objectType to a file
     '''
     with open(full_file_name, mode) as f:
-        if mode == 'w':
+        if mode == 'w' and imports:
             f.write(add_imports('import graphene'))
             f.write('\n\n')
-        f.write(objectType)
-        f.write('\n\n')
-    return f
+        for object in objecttype:
+            f.write(object)
+            f.write('\n\n')
